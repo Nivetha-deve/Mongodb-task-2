7fb9f5f0ac541672d90ee448cd2c93b341427432
Nivetha-deve <nivethashree028@gmail.com>
2024-06-01 12:27:47 +0530

    first commit

---
+++ Student-details.js
@@ -0,0 +1,234 @@
+{
+  "users": [
+    {
+      "user_id": 1,
+      "user_name": "Nivetha shree",
+      "user_email": "nivetha@gmail.com",
+      "user_phone_number": "1234567890",
+      "mentor_id": 101
+    },
+    {
+      "user_id": 2,
+      "user_name": "Swetha shree",
+      "user_email": "swetha@gmail.com",
+      "user_phone_number": "1238767890",
+      "mentor_id": 102
+    },
+    {
+      "user_id": 3,
+      "user_name": "kayalpavi shree",
+      "user_email": "kayal@gmail.com",
+      "user_phone_number": "1234567654",
+      "mentor_id": 103
+    },
+    {
+      "user_id": 4,
+      "user_name": "Nithya shree",
+      "user_email": "nithya@gmail.com",
+      "user_phone_number": "9144567890",
+      "mentor_id": 104
+    },
+    {
+      "user_id": 5,
+      "user_name": "vidhya shree",
+      "user_email": "vidhya@gmail.com",
+      "user_phone_number": "1245667890",
+      "mentor_id": 105
+    }
+  ],
+  "codekata": [
+    {
+      "session_id": 1,
+      "user_id": 1,
+      "date": "2020-10-01T00:00:00Z",
+      "problem_solved": "5"
+    },
+    {
+      "session_id": 2,
+      "user_id": 2,
+      "date": "2020-10-05T00:00:00Z",
+      "problem_solved": "10"
+    },
+    {
+      "session_id": 3,
+      "user_id": 3,
+      "date": "2020-10-20T00:00:00Z",
+      "problem_solved": "20"
+    },
+    {
+      "session_id": 4,
+      "user_id": 4,
+      "date": "2020-10-18T00:00:00Z",
+      "problem_solved": "9"
+    },
+    {
+      "session_id": 5,
+      "user_id": 5,
+      "date": "2020-10-28T00:00:00Z",
+      "problem_solved": "25"
+    }
+  ],
+  "attendance": [
+    {
+      "session_id": 1,
+      "user_id": 1,
+      "date": "2020-10-01T00:00:00Z",
+      "status": "present"
+    },
+    {
+      "session_id": 2,
+      "user_id": 2,
+      "date": "2020-10-05T00:00:00Z",
+      "status": "present"
+    },
+    {
+      "session_id": 3,
+      "user_id": 3,
+      "date": "2020-10-20T00:00:00Z",
+      "status": "absent"
+    },
+    {
+      "session_id": 4,
+      "user_id": 4,
+      "date": "2020-10-18T00:00:00Z",
+      "status": "present"
+    },
+    {
+      "session_id": 5,
+      "user_id": 5,
+      "date": "2020-10-05T00:00:00Z",
+      "status": "absent"
+    }
+  ],
+  "tasks": [
+    {
+      "task_id": 1,
+      "user_id": 1,
+      "task_description": "MongoDB assignment",
+      "due_date": "2020-10-20T00:00:00Z",
+      "status": "submitted"
+    },
+    {
+      "task_id": 2,
+      "user_id": 2,
+      "task_description": "sql bolt assignment",
+      "due_date": "2020-10-25T00:00:00Z",
+      "status": "submitted"
+    },
+    {
+      "task_id": 3,
+      "user_id": 3,
+      "task_description": "axios assignment",
+      "due_date": "2020-10-10T00:00:00Z",
+      "status": "not submitted"
+    },
+    {
+      "task_id": 4,
+      "user_id": 4,
+      "task_description": "router assignment",
+      "due_date": "2020-10-15T00:00:00Z",
+      "status": "not submitted"
+    },
+    {
+      "task_id": 5,
+      "user_id": 5,
+      "task_description": "node.js assignment",
+      "due_date": "2020-10-18T00:00:00Z",
+      "status": "submitted"
+    }
+  ],
+  "company_drives": [
+    {
+      "drive_id": 1,
+      "company_name": "NMN company",
+      "date": "2020-10-22T00:00:00Z"
+    },
+    {
+      "drive_id": 2,
+      "company_name": "BOSH company",
+      "date": "2020-10-29T00:00:00Z"
+    },
+    {
+      "drive_id": 3,
+      "company_name": "VVN company",
+      "date": "2020-10-13T00:00:00Z"
+    },
+    {
+      "drive_id": 4,
+      "company_name": "MLM company",
+      "date": "2020-10-02T00:00:00Z"
+    },
+    {
+      "drive_id": 5,
+      "company_name": "SNS company",
+      "date": "2020-10-12T00:00:00Z"
+    }
+  ],
+  "mentors": [
+    {
+      "mentor_id": 101,
+      "name": "akash",
+      "email": "akash@gmail.com",
+      "phone_number": "1234523434"
+    },
+    {
+      "mentor_id": 102,
+      "name": "pavithara",
+      "email": "pavithara@gmail.com",
+      "phone_number": "12345965334"
+    },
+    {
+      "mentor_id": 103,
+      "name": "kayal",
+      "email": "kayal@gmail.com",
+      "phone_number": "8906553587"
+    },
+    {
+      "mentor_id": 104,
+      "name": "swetha",
+      "email": "swetha@gmail.com",
+      "phone_number": "1237893434"
+    },
+    {
+      "mentor_id": 105,
+      "name": "akilh",
+      "email": "akilh@gmail.com",
+      "phone_number": "1765523434"
+    }
+  ],
+  "topics": [
+    {
+      "topic_id": 1,
+      "topic_name": "Introduction to MongoDB",
+      "description": "This topic covers the basics of MongoDB, including installation, basic commands, and CRUD operations.",
+      "date": "$2020-10-01T00:00:00Z"
+    },
+    {
+      "topic_id": 2,
+      "topic_name": "Advanced MongoDB",
+      "description": "This topic dives deeper into MongoDB features such as indexing, aggregation, and replication.",
+      "date": "2020-10-11T00:00:00Z"
+    },
+    {
+      "topic_id": 3,
+      "topic_name": "JavaScript Basics",
+      "description": "An introductory topic covering the basics of JavaScript, including variables, loops, and functions.",
+      "date": "2020-10-20T00:00:00Z"
+    },
+    {
+      "topic_id": 4,
+      "topic_name": "Node.js Fundamentals",
+      "description": "This topic introduces Node.js, explaining its core concepts, including modules, events, and file system interaction.",
+      "date": "2020-10-25T00:00:00Z"
+    },
+    {
+      "topic_id": 5,
+      "topic_name": "Express.js Basics",
+      "description": "A beginner's guide to Express.js, covering routing, middleware, and building simple web applications.",
+      "date": "2020-10-03T00:00:00Z"
+    }
+  ]
+}
+
+
+
